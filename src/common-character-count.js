5de3020accb1f12383460081a145ce1064bc08c5
AnatoliyIliev <vekok12@gmail.com>
2022-04-30 15:27:24 +0300

    finish: getCommonCharacterCount

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,40 +12,26 @@
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
+  const sOne = s1.split('');
+  const sTwo = s2.split('');
+  let result = 0;
 
-  
+  sOne.forEach(one => {
+    const seach = sTwo.indexOf(one);
 
-  // const sOne = s1.split('').sort();
-  // const sTwo = s2.split('').sort();
-  // let result = 0;
-  // console.log(sOne, sTwo);
-
-  // const recurs = (s, b) => {
-  //   const arr = [];
-  //   if (s.length > 0) {
-  //     const sFirstLetter = s.shift();
-  //     const find = b.indexOf(sFirstLetter);
-
-  //     console.log(find);
-  //     if (find !== -1) {
-  //       b.splice(find, 1);
-  //     }
-  //     console.log('s=', s, 'b=', b);
-  //     return recurs(s, arr);
-  //   }
-
-  //   return;
-  };
-
-  recurs(sOne, sTwo);
+    if (seach !== -1) {
+      sTwo.splice(seach, 1);
+      result += 1;
+    }
+  });
 
   return result;
 }
 
-// module.exports = {
-//   getCommonCharacterCount
-// };
-console.log(getCommonCharacterCount('aabcc', 'adcaa')); //, 3);
+module.exports = {
+  getCommonCharacterCount,
+};
+// console.log(getCommonCharacterCount('aabcc', 'adcaa')); //, 3);
 // console.log(getCommonCharacterCount('zzzz', 'zzzzzzz')); //, 4);
 // console.log(getCommonCharacterCount('abca', 'xyzbac')); //, 3);
 // console.log(getCommonCharacterCount('', 'abc')); //, 0);
