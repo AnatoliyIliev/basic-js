fd3f66f58bb295adc3a2f62fd2c6f4b5d0f34c0d
AnatoliyIliev <vekok12@gmail.com>
2022-05-01 21:22:21 +0300

    init: calculateHanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,24 +1,34 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Calculate turns number and time (in seconds) required
  * to solve puzzle
- * 
+ *
  * @param {Number} disks number of disks
  * @param {Number} turnsSpeed speed (in turns/hour)
  * @return {Object} object with props turns (number of turns)
  * and seconds (time in seconds)
  *
  * @example
- * 
+ *
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+  const speedSeconds = turnsSpeed / 60 / 60;
+  const turn = Math.pow(2, disksNumber) - 1;
+  return {
+    turns: turn,
+    seconds: Math.floor(turn / speedSeconds),
+  };
 }
 
-module.exports = {
-  calculateHanoi
-};
+// module.exports = {
+//   calculateHanoi
+// };
+
+// console.log(calculateHanoi(5, 4074)); //, { turns: 31, seconds: 27 });
+// console.log(calculateHanoi(38, 4594)); //, { turns: 274877906943, seconds: 215402800390 });
+// console.log(calculateHanoi(34, 4005)); //, { turns: 17179869183, seconds: 15442579040 });
+// console.log(calculateHanoi(19, 4770)); //, { turns: 524287, seconds: 395688 });
+// console.log(calculateHanoi(23, 4344)); //, { turns: 8388607, seconds: 6951884 });
