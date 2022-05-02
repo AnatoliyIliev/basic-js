a677a86314f74ed8f66a5db1d1502fc19e475499
AnatoliyIliev <vekok12@gmail.com>
2022-05-02 18:06:58 +0300

    init: DepthCalculator

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let newMatrix = matrix.map(i => i.slice()).map(i => i.fill(0));
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i > 0 && i < matrix.length - 1) {
+        if (matrix[i - 1][j - 1] === true) newMatrix[i][j] += 1;
+        if (matrix[i - 1][j] === true) newMatrix[i][j] += 1;
+        if (matrix[i - 1][j + 1] === true) newMatrix[i][j] += 1;
+        if (matrix[i + 1][j - 1] === true) newMatrix[i][j] += 1;
+        if (matrix[i + 1][j] === true) newMatrix[i][j] += 1;
+        if (matrix[i + 1][j + 1] === true) newMatrix[i][j] += 1;
+        if (matrix[i][j - 1] === true) newMatrix[i][j] += 1;
+        if (matrix[i][j + 1] === true) newMatrix[i][j] += 1;
+      } else if (i === 0) {
+        if (matrix[i + 1][j - 1] === true) newMatrix[i][j] += 1;
+        if (matrix[i + 1][j] === true) newMatrix[i][j] += 1;
+        if (matrix[i + 1][j + 1] === true) newMatrix[i][j] += 1;
+        if (matrix[i][j - 1] === true) newMatrix[i][j] += 1;
+        if (matrix[i][j + 1] === true) newMatrix[i][j] += 1;
+      } else if (i === matrix.length - 1) {
+        if (matrix[i - 1][j - 1] === true) newMatrix[i][j] += 1;
+        if (matrix[i - 1][j] === true) newMatrix[i][j] += 1;
+        if (matrix[i - 1][j + 1] === true) newMatrix[i][j] += 1;
+        if (matrix[i][j - 1] === true) newMatrix[i][j] += 1;
+        if (matrix[i][j + 1] === true) newMatrix[i][j] += 1;
+      }
+    }
+  }
+  return newMatrix;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
+
+// console.log(
+//   minesweeper([
+//     [true, false, false],
+//     [false, true, false],
+//     [false, false, false],
+//   ]),
+// );
+// [
+//   [1, 2, 1],
+//   [2, 1, 1],
+//   [1, 1, 1],
+// ],
+
+// console.log(
+//   minesweeper([
+//     [false, false, false],
+//     [false, false, false],
+//   ]),
+// );
+// [
+//   [0, 0, 0],
+//   [0, 0, 0],
+// ],
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,11 +1,11 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,12 +13,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  check = arr => arr.some(a => Array.isArray(a));
+
+  calculateDepth(arr) {
+    let result = 1;
+
+    if (this.check(arr)) {
+      const newArrey = arr.reduce((prev, current) => prev.concat(current), []);
+
+      return result + this.calculateDepth(newArrey);
+    }
+    return result;
   }
 }
 
 module.exports = {
-  DepthCalculator
+  DepthCalculator,
 };
+
+// const depthCalc = new DepthCalculator();
+// console.log(depthCalc.calculateDepth([[[]]]));
+// console.log(
+//   depthCalc.calculateDepth([
+//     1,
+//     [8, [[]]],
+//     [
+//       [
+//         [
+//           [
+//             [
+//               [
+//                 [
+//                   [
+//                     [
+//                       [[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]],
+//                       [],
+//                     ],
+//                   ],
+//                 ],
+//               ],
+//             ],
+//           ],
+//         ],
+//       ],
+//     ],
+//     2,
+//     3,
+//     [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]],
+//     [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]],
+//     4,
+//     5,
+//     ['6575', ['adas', ['dfg', [0]]]],
+//   ]),
+// ); //, 31);
